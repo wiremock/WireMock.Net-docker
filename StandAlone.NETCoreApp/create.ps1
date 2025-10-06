41bee7d7aa4c97afcf263c90c17a9bf3ba68ad07
Stef Heyenrath <stef.heyenrath@gmail.com>
2025-10-06 17:04:13 +0200

    1.14.0

--- StandAlone.NETCoreApp/create.ps1
+++ StandAlone.NETCoreApp/create.ps1
@@ -1,4 +1,4 @@
 docker build -t sheyenrath/wiremock.net -f .\Dockerfile .
-docker tag sheyenrath/wiremock.net:latest sheyenrath/wiremock.net:1.14.0-preview-01
+docker tag sheyenrath/wiremock.net:latest sheyenrath/wiremock.net:1.14.0
 docker rmi $(docker images -f "dangling=true" -q)
 docker images
